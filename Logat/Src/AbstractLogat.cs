0c1ad6c287be96306cae9e5a41082b08c7a88bfb
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-09-10 12:13:50 +0700

    AbstractLogat: add close method for releases the resources

--- Logat/Src/AbstractLogat.cs
+++ Logat/Src/AbstractLogat.cs
@@ -2,6 +2,11 @@ namespace TelkomDev.Logat
 {
     public abstract class AbstractLogat
     {
+        /// synchronize call to Log() method
+        /// ensure only one thread accesses Log() method
+        protected readonly object locker = new object();
         public abstract void Log(Level level, string message);
+
+        public abstract void Close();
     }
 }
