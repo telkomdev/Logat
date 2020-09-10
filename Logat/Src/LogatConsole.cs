85a8933033042ba59fd065d0347246924087c6c6
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-09-10 16:49:41 +0700

    remove unused import

--- Logat/Src/LogatConsole.cs
+++ Logat/Src/LogatConsole.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using Newtonsoft.Json;
 
 namespace TelkomDev.Logat
 {
--- Logat/Src/LogatFile.cs
+++ Logat/Src/LogatFile.cs
@@ -1,7 +1,6 @@
 using System;
 using System.IO;
 using System.Text;
-using Newtonsoft.Json;
 
 namespace TelkomDev.Logat
 {
