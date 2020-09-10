d6cca3d23c05a3a8abed958f7f92888cac87b61d
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-09-10 12:14:47 +0700

    LogatConsole: add override close method

--- Logat/Src/LogatConsole.cs
+++ Logat/Src/LogatConsole.cs
@@ -13,29 +13,37 @@ namespace TelkomDev.Logat
 
         public override void Log(Level level, string message)
         {
-            switch (level)
+            lock (locker)
             {
-                case Level.INFO:
-                    Console.ForegroundColor = ConsoleColor.White;
-                    break;
-                case Level.DEBUG:
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    break;
-                case Level.ERROR:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    break;
-                case Level.WARN:
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    break;
+                switch (level)
+                {
+                    case Level.INFO:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case Level.DEBUG:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case Level.ERROR:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case Level.WARN:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                }
+
+                var datetime = DateTime.Now.ToString("yyyy-mm-dd h:mm:ss");
+
+                var standardOutput = new StreamWriter(Console.OpenStandardOutput());
+                standardOutput.AutoFlush = true;
+                Console.SetOut(standardOutput);
+                Console.WriteLine("[{0}] {1} {2} {3}", appName, datetime, level.ToString(), message);
+                Console.ResetColor();
             }
+        }
 
-            var datetime = DateTime.Now.ToString("yyyy-mm-dd h:mm:ss");
-
-            var standardOutput = new StreamWriter(Console.OpenStandardOutput());
-            standardOutput.AutoFlush = true;
-            Console.SetOut(standardOutput);
-            Console.WriteLine("[{0}] {1} {2} {3}", appName, datetime, level.ToString(), message);
-            Console.ResetColor();
+        public override void Close()
+        {
+            
         }
     }
 }
