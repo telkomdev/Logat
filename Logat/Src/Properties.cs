751438a8acdf079c3653d8d90f895947a2e191b4
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-09-10 11:29:06 +0700

    Properties: add Format properties

--- Logat/Src/Properties.cs
+++ Logat/Src/Properties.cs
@@ -2,16 +2,24 @@ using System;
 
 namespace TelkomDev.Logat
 {
-    public enum Target: byte {
+    public enum Target: byte 
+    {
         Console,
         File,
         Logstash
     }
 
-    public enum Level: byte {
+    public enum Level: byte 
+    {
         INFO,
         DEBUG,
         ERROR,
         WARN
+    }
+
+    public enum Format: byte 
+    {
+        PLAIN,
+        JSON
     }
 }
