36b96693c025432583906829bcaf8a66c34fe008
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-09-10 12:15:31 +0700

    LogatFile: add basic write stream (wip)

--- Logat/Src/Logat.cs
+++ Logat/Src/Logat.cs
@@ -4,6 +4,9 @@ namespace TelkomDev.Logat
 {
     public class Logat
     {
-        
+        public Logat(AbstractLogat targetLogat)
+        {
+
+        }
     }
 }
--- Logat/Src/LogatFile.cs
+++ Logat/Src/LogatFile.cs
@@ -1,16 +1,47 @@
+using System;
+using System.IO;
+
 namespace TelkomDev.Logat
 {
-    class LogatFile : AbstractLogat
+    public class LogatFile : AbstractLogat
     {
         private string appName;
-        public LogatFile(string appName)
+        private string fileName;
+
+        private StreamWriter fileOutput;
+        public LogatFile(string appName, string fileName)
         {
             this.appName = appName;
+            this.fileName = fileName;
+
+            try
+            {
+                fileOutput = new StreamWriter(fileName, true);
+            }
+            catch (Exception e)
+            {
+                throw new IOException(e.ToString());
+            }
         }
 
         public override void Log(Level level, string message)
         {
-            throw new System.NotImplementedException();
+            lock (locker)
+            {
+                var datetime = DateTime.Now.ToString("yyyy-mm-dd h:mm:ss");
+
+                ///fileOutput.AutoFlush = true;
+                Console.SetOut(fileOutput);
+                Console.WriteLine("[{0}] {1} {2} {3}", appName, datetime, level.ToString(), message);
+            }
+        }
+
+        public override void Close()
+        {
+            if (fileOutput != null)
+            {
+                fileOutput.Close();
+            }
         }
     }
 }
